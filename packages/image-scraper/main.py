15a1e60e2fde05553fa5e0e992825e39671f73d0
Jesse Snyder <jessetsnyder@gmail.com>
2021-09-05 22:39:05 -0600

    add user agent header to phantomjs driver

--- packages/image-scraper/main.py
+++ packages/image-scraper/main.py
@@ -1,21 +1,31 @@
-import requests
+import os
 from datetime import date
 from urllib.parse import urlencode
 
+import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver import DesiredCapabilities
 
 
-def get_html(url) -> str:
+def get_html(url: str) -> str:
     """
     Returns the HTML for a given webpage.
     """
-    browser = webdriver.PhantomJS()
+    desired_capabilities = DesiredCapabilities.PHANTOMJS.copy()
+    desired_capabilities['phantomjs.page.customHeaders.User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) ' \
+                                                                      'AppleWebKit/537.36 (KHTML, like Gecko) ' \
+                                                                      'Chrome/39.0.2171.95 Safari/537.36'
+    browser = webdriver.PhantomJS(desired_capabilities=desired_capabilities)
     browser.get(url)
     html = browser.page_source
     return html
 
-def get_soup(url):
+
+def get_soup(url: str):
+    """
+    Returns object describing content downloaded from url.
+    """
     html = get_html(url)
     soup = BeautifulSoup(html, 'html.parser')
     return soup
@@ -30,7 +40,9 @@ def get_daily_search_term() -> str:
 
     parts = todays_header.text.split('-')
     search_term = parts[1].strip()
+    print(f'search term for today: {search_term}')
     return search_term
+
 
 def scrape_top_100_results(search_term: str = get_daily_search_term()) -> None:
     """
@@ -48,12 +60,17 @@ def scrape_top_100_results(search_term: str = get_daily_search_term()) -> None:
     soup = get_soup(images_url)
     image_items = soup.find_all('img', 'tile--img__img')
 
+    # create directory for today's date
+    date_string = str(date.today())
+    todays_directory = os.path.join('./scrapes', date_string)
+    os.makedirs(todays_directory)
+
     for item in image_items:
         image_source = f"https:{item['src']}"
+        print(f'downloading {image_source}')
         res = requests.get(image_source)
 
-        date_string = str(date.today())
-        filename = f"{date_string}/scrapes/{item['alt']}"
+        filename = f"scrapes/{date_string}/{item['alt']}"
         file = open(filename, 'wb')
         file.write(res.content)
         file.close()
